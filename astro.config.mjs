6ea7aea9af0a37ce3784e7190a1e1bb53e9144aa
idealelectricalsolution-wq <idealelectricalsolution@gmail.com>
2026-05-17 17:31:12 +0100

    build.format: file - produce flat .html files to fix 308 redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://liverpoolemergencyplumber.co.uk',
   trailingSlash: 'never',
+  build: {
+    format: 'file'
+  },
   integrations: [sitemap()],
 });
